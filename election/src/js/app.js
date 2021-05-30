c76a76db207964750b703665ccc5991db5251ca2
sener7106 <dskbbsm@gmail.com>
2021-05-30 17:18:43 +0900

    app.js 수정
    app.js내 에서 프론트엔드 후보자화면을 표시

--- election/src/js/app.js
+++ election/src/js/app.js
@@ -1,68 +1,99 @@
-App = {
-  web3Provider: null,
-  contracts: {},
+  App = {
 
-  init: async function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+    web3Provider:null ,
+    contracts: {},
+    account:'0x0',
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+    init: function(){
+      return App.initWeb3();
+    },
 
-        petsRow.append(petTemplate.html());
+    initWeb3: function(){
+      if (typeof web3 !== 'undefined') {
+        // If a web instance is already provided by metamask.
+        // 만약 웹 환경이 이미 메타마스크에 제공되고 있는 경우.
+        App.web3Provider = web3.currentProvider;
+        web3 = new Web3(web3.currentProvider);
+      } else {
+        // Spectify default instance if no web3 instance provided
+        // 인스턴스가 실행되고 있지 않은 경우
+        App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+        web3 = new Web3(App.web3Provider);
       }
+      return App.initContract();
+    },
+
+    initContract: function() {
+      $.getJSON("Election.json", function(election) {
+        //$("_cmt" + seq)는document.all.("_cmt" + seq) 
+        //Instantiate a new truffle contract from the artifact 
+        //스마트계약 실행
+        App.contracts.Election = TruffleContract(election);
+        // Connect provider to interact with contract
+        App.contracts.Election.setProvider(App.web3Provider);
+
+        return App.render();
+      });
+    },
+
+    render: function() {
+      var electionInstance;
+      var loader = $("#loader");
+      var content = $("#content");
+      // index.html의 loader와 content와 연결
+
+      loader.show();
+      content.hide();
+      // rendering되기 전까지 loader화면은 보이고, content화면은 숨김
+    
+      // Load account data
+      web3.eth.getCoinbase(function(err, account) {
+        if (err === null) {
+          App.account = account;
+          $("#accountAddress").html("Your Account : " + account);
+          // error가 없을 경우, html에 account주소를 표기
+          // 이더 계정을 연동
+        }
+      });
+
+      
+
+      // Load Contract data
+
+      App.contracts.Election.deployed().then(function(instance) {
+        electionInstance = instance;
+        return electionInstance.candidatesCount();
+      }).then(function(candidatesCount){
+        var candidatesResults = $("#candidatesResults");
+        candidatesResults.empty();
+        // candidatesResult를 index.html에 표시. 
+      
+        for (var i = 1; i <= candidatesCount; i++){
+
+          electionInstance.candidates(i).then(function(candidate) {
+            var id = candidate[0];
+            var name = candidate[1];
+            var voteCount = candidate[2];
+         
+        // 후보자 수 만큼 electionInstance에 후보자 데이터를 저장
+        // Render candidate Result
+          var candidateTemplate = "<tr><th>" + id + "</th><td>" + name + "</td><td>" + voteCount + "</td></tr>"
+          candidatesResults.append(candidateTemplate);
+        // 후보자 템플릿에 id, name, votecount를 추가
+         });
+        }
+
+        loader.hide();
+        content.show();
+      }).catch(function(error) {
+        console.warn(error);
+        alert("error");
+      });
+    }
+  };
+
+  $(function() {
+    $(window).load(function() {
+      App.init();
     });
-
-    return await App.initWeb3();
-  },
-
-  initWeb3: async function() {
-    /*
-     * Replace me...
-     */
-
-    return App.initContract();
-  },
-
-  initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
-  },
-
-  bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
-  },
-
-  markAdopted: function() {
-    /*
-     * Replace me...
-     */
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
-};
-
-$(function() {
-  $(window).load(function() {
-    App.init();
   });
-});
